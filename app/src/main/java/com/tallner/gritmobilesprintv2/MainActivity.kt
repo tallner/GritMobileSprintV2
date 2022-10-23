fc2cbe94d061e22f4c69ea97d87ae44d5e92ee4b
tallner <ctallner@hotmail.com>
2022-10-23 16:31:19 +0200

    topbar with username and icon added

--- app/src/main/java/com/tallner/gritmobilesprintv2/MainActivity.kt
+++ app/src/main/java/com/tallner/gritmobilesprintv2/MainActivity.kt
@@ -2,10 +2,17 @@ package com.tallner.gritmobilesprintv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.title = ""
     }
 }
