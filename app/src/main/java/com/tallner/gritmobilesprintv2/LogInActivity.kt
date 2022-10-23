f93fb08bd17311d76209d4cdcd7d3db5670373ea
tallner <ctallner@hotmail.com>
2022-10-23 15:07:18 +0200

    Basic program layout

---
+++ app/src/main/java/com/tallner/gritmobilesprintv2/LogInActivity.kt
@@ -0,0 +1,11 @@
+package com.tallner.gritmobilesprintv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
---
+++ app/src/main/java/com/tallner/gritmobilesprintv2/NewUserActivity.kt
@@ -0,0 +1,11 @@
+package com.tallner.gritmobilesprintv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_user)
+    }
+}
