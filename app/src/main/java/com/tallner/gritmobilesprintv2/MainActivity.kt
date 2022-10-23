21f91c6191d063e7c16c3b518c0672c764dc8c28
tallner <ctallner@hotmail.com>
2022-10-23 17:52:15 +0200

    fragments navigation implemented

--- app/src/main/java/com/tallner/gritmobilesprintv2/MainActivity.kt
+++ app/src/main/java/com/tallner/gritmobilesprintv2/MainActivity.kt
@@ -3,16 +3,35 @@ package com.tallner.gritmobilesprintv2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val toolbar: Toolbar = findViewById(R.id.toolbar_main)
         setSupportActionBar(toolbar)
 
         supportActionBar!!.title = ""
+
+        //initiate the bottom navigation component
+        val btmNavView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        //setup the fragment manager
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //setup the bar on top of the app to get the labels from the fragments
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.chatFragment,R.id.searchFragment,R.id.settingsFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        //setup the navigation bar to be used with the navigation controller
+        btmNavView.setupWithNavController(navController)
     }
 }
