035f43bc516c400ea26c92ed00db9b3d67036708
tallner <ctallner@hotmail.com>
2022-10-25 15:04:46 +0200

    Navigation in startactivity added

--- app/src/main/java/com/tallner/gritmobilesprintv2/StartActivity.kt
+++ app/src/main/java/com/tallner/gritmobilesprintv2/StartActivity.kt
@@ -1,11 +1,46 @@
 package com.tallner.gritmobilesprintv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
+
+    private lateinit var firebaseUser : FirebaseUser
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        btn_login.setOnClickListener {
+            val i = Intent(this@StartActivity,LogInActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+        btn_register.setOnClickListener {
+            val i = Intent(this@StartActivity,NewUserActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser!!
+
+        //if there is a user logged in then go to mainactivity
+        if (firebaseUser != null) {
+            val i = Intent(this@StartActivity,MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
