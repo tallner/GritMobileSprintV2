b5027e2d97cb563291535d21d6bb251797fb0202
tallner <ctallner@hotmail.com>
2022-10-26 13:28:39 +0200

    user model class added

---
+++ app/src/main/java/com/tallner/gritmobilesprintv2/models/User.kt
@@ -0,0 +1,31 @@
+package com.tallner.gritmobilesprintv2.models
+
+/*
+class User(
+    val uid: String,
+    val username: String,
+    val profile: String,
+    val cover: String,
+    val status: String,
+    val search: String,
+    val facebook: String,
+    val instagram: String,
+    val website: String)
+{
+    constructor(): this("","", "","","","","",
+        "", "")
+}
+*/
+
+class User {
+    constructor()
+    constructor(uid: String,
+                username: String,
+                profile: String,
+                cover: String,
+                status: String,
+                search: String,
+                facebook: String,
+                instagram: String,
+                website: String)
+}
