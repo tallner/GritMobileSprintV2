1d9b7f0a6c763e8553207f2d4ea34588768fd403
tallner <ctallner@hotmail.com>
2022-11-03 18:44:55 +0100

    Documentation added

--- app/src/main/java/com/tallner/gritmobilesprintv2/fragments/ChatFragment.kt
+++ app/src/main/java/com/tallner/gritmobilesprintv2/fragments/ChatFragment.kt
@@ -8,24 +8,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
-import com.google.firebase.database.ktx.getValue
-import com.squareup.picasso.Picasso
 import com.tallner.gritmobilesprintv2.R
 import com.tallner.gritmobilesprintv2.adapters.ChatAdapter
-import com.tallner.gritmobilesprintv2.adapters.UserAdapter
 import com.tallner.gritmobilesprintv2.models.Chat
-import com.tallner.gritmobilesprintv2.models.User
 import kotlinx.android.synthetic.main.fragment_chat.*
-import kotlinx.android.synthetic.main.fragment_chat.view.*
-import org.checkerframework.checker.units.qual.C
 
 class ChatFragment : Fragment() {
 
@@ -65,7 +57,6 @@ class ChatFragment : Fragment() {
         if (receiverID=="null") {
             receiverID = sharedPreferences.getString(USERID_KEY, null)!!
         }
-        Log.i("mytag", receiverID.toString())
 
 
         btn_send = view.findViewById(R.id.btn_sendmessage)
@@ -97,55 +88,9 @@ class ChatFragment : Fragment() {
 
         readMessages(senderID,receiverID)
 
-      //  Log.i("mytag", "nr chats " + (myChats.size-1).toString())
-
-
         return  view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-    }
-/*
-    private fun getChatUser(userID:String){
-        if (userID.equals(null)) return
-
-        val refUser = FirebaseDatabase
-            .getInstance(databaseURL)
-            .getReference("Users")
-            .child(userID)
-
-
-
-        refUser.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-                val user:User? = snapshot.getValue(User::class.java)
-
-                if (user != null){
-                    tv_username.text = user.username
-                    if (user.profile == "") {
-                        icon_user.setImageResource(R.drawable.profile)
-                    } else {
-                        if (user != null) {
-                            Picasso.get().load(user.profile).placeholder(R.drawable.profile)
-                                .into(icon_user)
-                        }
-                    }
-                }
-            }
-
-
-            override fun onCancelled(error: DatabaseError) {
-            }
-
-
-        })
-
-    }
-*/
     private fun sendMessage(senderID:String,receiverID:String,message:String){
         val refChat = FirebaseDatabase.getInstance(databaseURL).reference.child("Chat")
 
@@ -160,22 +105,7 @@ class ChatFragment : Fragment() {
     }
 
     private fun scrollToBottom(){
-        //val refChat = FirebaseDatabase.getInstance(databaseURL).getReference("Chat")
         recyclerview?.scrollToPosition(myChats.size-1)
-        Log.i("mytag", "nr chats " + (myChats.size-1).toString())
-/*
-        refChat.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
-
- */
     }
 
     private fun readMessages(senderID:String,receiverID:String){
